e20d57902b6a4670eccf8545e2b6bc28e8109295
thole <thole@thole>
2025-01-28 16:30:16 +0100

    part 1 almost done, issue with token and curl

--- p1/scripts/agent.sh
+++ p1/scripts/agent.sh
@@ -1,1 +1,19 @@
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent" K3S_TOKEN="mypassword" K3S_URL=https://192.168.56.110:6443 sh - || echo "Failed to install K3s agent"
+#!/bin/bash
+
+# Variables
+MASTER_IP="192.168.56.110"
+apt-get update
+apt-get install -y curl
+# Fetch the token from the master
+echo "Fetching token from the master node..."
+TOKEN=$(cat /tmp/confs/token)
+if [ -z "$TOKEN" ]; then
+    echo "Failed to retrieve the token from the master node."
+    exit 1
+fi
+
+# Install k3s agent
+echo "Installing k3s agent..."
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent" \
+    K3S_TOKEN="$TOKEN" \
+    K3S_URL="https://$MASTER_IP:6443" sh - || echo "Failed to install K3s agent"
--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,1 +1,4 @@
+apt-get update
+apt-get install -y curl
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" K3S_TOKEN=12345 sh -s || echo "Failed to install K3s server"
+mv /var/lib/rancher/k3s/server/token /tmp/confs/
