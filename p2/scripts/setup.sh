efcfb76607b4734e63e5643a8028be3c8013d9a3
FrenchDandelions <thomas.le.tle@gmail.com>
2025-05-26 17:36:41 +0200

    checking something

--- p2/scripts/setup.sh
+++ p2/scripts/setup.sh
@@ -3,5 +3,5 @@
 apt-get update
 apt-get install -y curl
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s || echo "Failed to install K3s server"
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip 192.168.56.110" sh -s || echo "Failed to install K3s server"
 sudo kubectl apply -f ./remote/confs
