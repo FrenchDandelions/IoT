792fcafc0e48ff9b428d37c5d7142aafb918bd29
FrenchDandelions <thomas.le.tle@gmail.com>
2025-06-03 18:03:07 +0200

    fixed the wait

--- p3/scripts/run.sh
+++ p3/scripts/run.sh
@@ -10,6 +10,7 @@ k3d cluster create $CLUSTER_NAME -p $CLUSTER_PORT:$CLUSTER_PORT -p $APP_PORT
 
 kubectl get ns $NAMESPACE_ARGOCD || kubectl create namespace $NAMESPACE_ARGOCD
 kubectl apply -n $NAMESPACE_ARGOCD -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl -n $NAMESPACE_ARGOCD wait --for=condition=available --timeout=300s deployment argocd-server
 kubectl apply -n $NAMESPACE_ARGOCD -k patchs
 kubectl apply -n $NAMESPACE_ARGOCD -f confs
 
