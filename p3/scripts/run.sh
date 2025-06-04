1f673af274816969289f81a5044fb5c2f9b688fb
FrenchDandelions <thomas.le.tle@gmail.com>
2025-06-04 19:54:49 +0200

    tried again for the 4th time to fix the sync refresh to every 15sec instead of every 3min

--- p3/scripts/run.sh
+++ p3/scripts/run.sh
@@ -21,7 +21,7 @@ kubectl patch deployment argocd-server -n $NAMESPACE_ARGOCD --type='json' -p='[{
 kubectl -n $NAMESPACE_ARGOCD wait --for=condition=available --timeout=300s deployment argocd-server
 
 # Patch argocd-application-controller to reduce repo cache TTL to 30s
-kubectl patch deployment argocd-repo-server -n argocd --type='json' -p='[{"op":"add","path":"/spec/template/spec/containers/0/args/-","value":"--repo-cache-expiration=15s"}]'
+kubectl patch deployment argocd-repo-server -n $NAMESPACE_ARGOCD --type='json' -p='[{"op":"add","path":"/spec/template/spec/containers/0/args/-","value":"--repo-cache-expiration=15s"}]'
 
 kubectl -n $NAMESPACE_ARGOCD wait --for=condition=available --timeout=300s deployment argocd-server
 
